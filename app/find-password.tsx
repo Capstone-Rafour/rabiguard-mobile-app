8f6a3dfb477aec6ce7ff4066c0c801e77d578b52
pocariri <wsdvbn011@gmail.com>
2026-04-30 11:32:29 +0900

    비밀번호 찾기 화면 UI 구현

--- app/find-password.tsx
+++ app/find-password.tsx
@@ -2,19 +2,18 @@ import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import React, { useState } from "react";
 import {
-  KeyboardAvoidingView,
-  Platform,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
+    KeyboardAvoidingView,
+    Platform,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
 } from "react-native";
 import ScreenContainer from "../components/screen-container";
 
-export default function LoginScreen() {
+export default function FindPasswordScreen() {
   const router = useRouter();
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
   return (
     <ScreenContainer>
@@ -28,45 +27,26 @@ export default function LoginScreen() {
             <Ionicons name="chevron-back" size={28} color="black" />
           </TouchableOpacity>
 
-          <Text className="text-3xl font-bold mb-4">기존 계정으로 로그인</Text>
+          <Text className="text-3xl font-bold mb-4">비밀번호 찾기</Text>
 
           {/* 이메일 입력 창 */}
           <View className="space-y-4">
             <TextInput
-              placeholder="이메일"
+              placeholder="이메일 주소"
               value={email}
               onChangeText={setEmail}
               className="w-full h-14 bg-gray-200 rounded-xl px-4 text-lg"
               keyboardType="email-address"
               autoCapitalize="none"
             />
-
-            {/* 비밀번호 입력 창 */}
-            <TextInput
-              placeholder="비밀번호"
-              value={password}
-              onChangeText={setPassword}
-              className="w-full h-14 bg-gray-200 rounded-xl px-4 text-lg mt-4"
-              secureTextEntry
-            />
           </View>
 
-          {/* 로그인 버튼 */}
+          {/* 인증 코드 받기 버튼 */}
           <TouchableOpacity
             className="w-full h-14 bg-[#5D60F1] rounded-xl justify-center items-center mt-12"
-            onPress={() => router.push("/home")}
+            onPress={() => router.push("/verification")}
           >
-            <Text className="text-white text-lg font-bold">
-              기존 계정으로 로그인
-            </Text>
-          </TouchableOpacity>
-
-          {/* 비밀번호 찾기 */}
-          <TouchableOpacity
-            className="mt-4 items-center"
-            onPress={() => router.push("/find-password")}
-          >
-            <Text className="text-[#5D60F1] text-base">비밀번호 찾기</Text>
+            <Text className="text-white text-lg font-bold">인증 코드 받기</Text>
           </TouchableOpacity>
         </View>
       </KeyboardAvoidingView>
