8d29f9aa359d05d0466ade67710d215d9d4942e6
pocariri <wsdvbn011@gmail.com>
2026-05-14 13:04:02 +0900

    fix: 안드로이드 초기 진입 시 라우팅 로직 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,32 @@
+import { useColorScheme } from "@/hooks/use-color-scheme";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "react-native-reanimated";
 import "../global.css";
 
-import { useColorScheme } from "@/hooks/use-color-scheme";
+SplashScreen.preventAutoHideAsync();
 
-SplashScreen.preventAutoHideAsync;
-
-export const unstable_settings = {
-  anchor: "(tabs)",
-};
+// export const unstable_settings = {
+//   anchor: "(tabs)",
+// };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const segments = useSegments();
+  const router = useRouter();
+
+  // 스플래시 제어용 로딩 상태 관리
+  const [isReady, setIsReady] = useState(false);
+
+  // 로그인 상태 확인
+  const isLoggedIn = false; // 실제 로그인 상태로 대체
 
   useEffect(() => {
     async function prepare() {
@@ -34,9 +41,34 @@ export default function RootLayout() {
     prepare();
   }, []);
 
+  // 인증 로직(로그인 여부에 따른 라우팅)
+  useEffect(() => {
+    if (!isReady) return;
+
+    const authScreens = [
+      "index",
+      "login",
+      "sign-up",
+      "verification",
+      "find-password",
+    ];
+    const isAuthScreen = authScreens.includes(segments[0] as string);
+
+    if (!isLoggedIn && !isAuthScreen) {
+      // 로그인 안 됐는데 서비스 내부(tabs 등)로 들어가려 하면?
+      // 가장 첫 화면인 index로 튕겨냅니다.
+      router.replace("/");
+    } else if (isLoggedIn && isAuthScreen) {
+      // 로그인 됐는데 로그인 화면이나 index에 머물러 있다면?
+      // 메인 홈으로 보냅니다.
+      router.replace("/(tabs)/home" as any);
+    }
+  }, [isLoggedIn, segments, isReady]);
+
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen
           name="modal"
